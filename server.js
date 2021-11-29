ca738ef666d98fe16215e68438793ea8a020b066
Madhav V Rajeev <75824625+GodOfCoding1@users.noreply.github.com>
2021-11-29 23:07:35 +0530

    fixed stuff

--- server.js
+++ server.js
@@ -13,9 +13,7 @@ app.use(function(req, res, next) {
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.get("/resume", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "madhav-rajeev.pdf"));
-});
+
 // send the user to index html page inspite of the url
 app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "index.html"));

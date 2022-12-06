619a41819b208f7507ee25f5d65f9d4c830fcbc5
Madhav V Rajeev <75824625+GodOfCoding1@users.noreply.github.com>
2022-12-06 17:36:25 +0530

    some changes

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(function(req, res, next) {
-    if (req.header("x-forwarded-proto") !== "https") {
-        res.redirect("https://" + req.header("host") + req.baseUrl);
-    } else {
-        next();
-    }
+app.use(function (req, res, next) {
+  if (req.header("x-forwarded-proto") !== "https") {
+    res.redirect("https://" + req.header("host") + req.baseUrl);
+  } else {
+    next();
+  }
 });
 
 // the __dirname is the current directory from where the script is running
@@ -16,7 +16,7 @@ app.use(express.static(__dirname));
 
 // send the user to index html page inspite of the url
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "index.html"));
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.listen(port);

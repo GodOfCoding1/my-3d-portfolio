9b49405275c8efa05ef0786da1181f23e6b1c792
Madhav V Rajeev <75824625+GodOfCoding1@users.noreply.github.com>
2022-12-06 18:55:34 +0530

    vercel deploy

---
+++ build/src/resources/textures.js
@@ -0,0 +1,59 @@
+//billboardTextures
+let billboardTextures = {};
+billboardTextures.terpSolutionsTexture = "../src/jsm/koyo.png";
+billboardTextures.bagHolderBetsTexture = "../src/jsm/explified.png";
+billboardTextures.homeSweetHomeTexture = "../src/jsm/coinface.png";
+
+//box textures
+let boxTexture = {};
+boxTexture.Github = "../src/jsm/githubLogo.png";
+boxTexture.twitter = "../src/jsm/twitter.png";
+boxTexture.LinkedIn = "../src/jsm/linkedInLogo.png";
+boxTexture.mail = "../src/jsm/envelope.png";
+boxTexture.globe = "../src/jsm/thunder.png";
+boxTexture.reactIcon = "../src/jsm/react.png";
+boxTexture.allSkills = "../src/jsm/allSkills.png";
+boxTexture.lensFlareMain = "../src/jsm/lensflare0.png";
+boxTexture.skrillex = "../src/jsm/skrillex.png";
+boxTexture.edmText = "../src/jsm/EDM.png";
+boxTexture.resume = "../src/jsm/resume.png";
+
+//material textures
+let stoneTexture = "../src/jsm/stone.png";
+let woodTexture = "../src/jsm/woodTexture.jpg";
+
+//text
+let inputText = {};
+inputText.terpSolutionsText = "../src/jsm/hello.png";
+inputText.activities = "../src/jsm/activities_text.png";
+inputText.bagholderBetsText = "../src/jsm/explified-text.png";
+inputText.homeSweetHomeText = "../src/jsm/coinface-text.png";
+inputText.staticPortfolio = "../src/jsm/static-portfolio.png";
+
+//SVG
+let SVG = {};
+SVG.reactLogo = "../src/jsm/react-svg.svg";
+
+//URLs
+let URL = {};
+URL.terpsolutions = "https://www.koyosolutions.com/";
+URL.ryanfloyd = "https://ryanfloyd.io";
+URL.bagholderBets = "https://explified.com/";
+URL.homeSweetHomeURL = "https://coin-face.herokuapp.com/";
+URL.gitHub = "https://github.com/GodOfCoding1";
+URL.twitter = "https://twitter.com/0xFloyd";
+URL.LinkedIn = "https://www.linkedin.com/in/madhav-rajeev-b9485a205/";
+URL.email = "https://mailto:madhavrajeev303@gmail.com";
+URL.githubBagholder = "https://explified.com/";
+URL.githubHomeSweetHome = "https://coin-face.herokuapp.com/";
+URL.devTo =
+    "https://dev.to/mrryanfloyd/create-an-interactive-3d-portfolio-website-that-stands-out-to-employers-47gc";
+
+export {
+    billboardTextures,
+    boxTexture,
+    inputText,
+    URL,
+    stoneTexture,
+    woodTexture,
+};

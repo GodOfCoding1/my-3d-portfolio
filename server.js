520fc159a30bba3f9da76589b89d60560da47929
Madhav V Rajeev <75824625+GodOfCoding1@users.noreply.github.com>
2022-12-06 18:22:29 +0530

    heoku deployment

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(function (req, res, next) {
-  if (req.header("x-forwarded-proto") !== "https") {
-    res.redirect("https://" + req.header("host") + req.baseUrl);
-  } else {
-    next();
-  }
-});
+// app.use(function (req, res, next) {
+//   if (req.header("x-forwarded-proto") !== "https") {
+//     res.redirect("https://" + req.header("host") + req.baseUrl);
+//   } else {
+//     next();
+//   }
+// });
 
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
